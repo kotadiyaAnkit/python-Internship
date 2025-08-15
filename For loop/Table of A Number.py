f62f1705ae4b8186b509843b7ec117e28fb398de
KotadiyaAnkit <ankitkotadiya993@gmail,com>
2025-08-15 10:52:20 +0530

    day4

--- For loop/Table of A Number.py
+++ For loop/Table of A Number.py
@@ -1,17 +1,17 @@
-#First, let's start with a basic program that generates the multiplication table for a given number n. This program will print the first 10 multiples of n.
-n = int(input("Enter a number to generate its multiplication table: "))
-i = int(input("Enter the range for the multiplication table (default is 10): "))
-for i in range(1, 11):
-    print(n * i, end=" ")
+#User function Template for python3
+n1 = int(input())
+n2 = int(input())
 
-print("\n---------------------------------------------")
-#Extended Multiplication Table with User-Specified Length Using while Loop
-#Next, let's extend the program to allow the user to specify how many multiples they want to print.
+for x in range(1,11):
+    print(n1*x, end=" ")
+    
+    
+for i in range(1,11):
+    print(n2*i, end=" ")
 
-a=5
-b=7
-c=1
+d=(n1*x)
+e=(n2*i)
+print(d-e)
 
-while c<=b:
-    print(a*c, end=" ")
-    c+=1
+    
+# Your code here
---
+++ For loop/Two_Difference.py
@@ -0,0 +1,30 @@
+#Given two number n1 and n2, n1 > n2. Find the differences between mathematical tables of n1 and n2 and print in a single line.
+#Note: Don't add a new line in the end.
+
+n1 = int(input()) #int() converts that string into an integer. Example: If the user enters 9, then n1 = 9.
+n2 = int(input())
+
+result = [str(n1 * i - n2 * i) for i in range(1, 11)] #For each i, we calculate: n1 * i and n2 * i, then subtract them: n1*i - n2*i.
+
+
+print(" ".join(result))
+
+print("-------------------------------")
+n1 = int(input())
+n2 = int(input())
+
+table_n1 = [n1 * i for i in range(1, 11)]
+table_n2 = [n2 * i for i in range(1, 11)]
+diff = [table_n1[i] - table_n2[i] for i in range(10)]
+
+# Print first line: table of n1
+print(" ".join(map(str, table_n1)))
+
+# Print second line: table of n2
+print("- " + " ".join(map(str, table_n2)))
+
+# Print separator line
+print("-" * 41)
+
+# Print result line: differences
+print("= " + " ".join(map(str, diff)), end="")
