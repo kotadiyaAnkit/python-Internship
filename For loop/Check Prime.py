efc0613a8ca5a37888d894b9f52f78872d654eb8
KotadiyaAnkit <ankitkotadiya993@gmail,com>
2025-08-25 11:44:57 +0530

    Check Prime_num

---
+++ For loop/Check Prime.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+n = int(input())
+
+if n<=1:   #We check n <= 1: Not prime → print "False"
+    print("False")
+else:     
+    for i in range(2 , int(n **0.5) +1):
+        if n % i ==0:    #If n % i == 0: Not prime → print "False" and stop
+            print("False")
+            break
+    else:
+        print("True")
+    
+
+# Your code here
