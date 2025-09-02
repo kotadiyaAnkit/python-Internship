a31d6862c1a3fa6b8683647423d3a51d51db1c38
KotadiyaAnkit <ankitkotadiya993@gmail,com>
2025-09-02 10:43:32 +0530

    string Exercise

--- string method/Binary number to decimal number.py
+++ string method/Binary number to decimal number.py
@@ -1,6 +1,10 @@
 class Solution:
     def binaryToDecimal(self, b):
-        return int(b, 2)
+        return int(b, 2) #int(b, 2) tells Python to convert the string b (which is in base-2) into a base-10 integer.
+    #int("111", 2) → 7
+    #int("1010", 2) → 10
+    #int("100", 2) → 4
 
 s1 = Solution()
 print(s1.binaryToDecimal("111"))
+print(s1.binaryToDecimal("1010"))
